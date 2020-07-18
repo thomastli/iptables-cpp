8fb2dab3194e2d7c8e489ab639c93cc3bbbf267a
Thomas Li <thomas.tong.li@gmail.com>
2020-07-18 20:55:50 -0300

    Added libiptc headers for C/C++ API, implemented various iptables commands in IpTablesWrapper.cpp

--- include/iptables/IpTablesWrapper.hpp
+++ include/iptables/IpTablesWrapper.hpp
@@ -1,6 +1,9 @@
 #ifndef IP_TABLES_WRAPPER_HPP
 #define IP_TABLES_WRAPPER_HPP
 
+#include <libiptc/libiptc.h>
+
+#include "iptables/Chain.hpp"
 #include "iptables/Rule.hpp"
 #include "iptables/RuleMap.hpp"
 
@@ -20,33 +23,68 @@ class IpTablesWrapper {
   ~IpTablesWrapper();
 
   /**
-   * @brief List all ip rules in the ip tables map
+   * @brief Initialize the ip tables module
    *
+   * @param tableName
    */
-  void listRulesFromIpTable();
+  void initializeIpTablesModule();
 
   /**
-   * @brief Add an Rule to the ip tables map
+   * @brief Cleanup the iptables module
    *
-   * @param rule    Rule object
    */
-  void addRuleToIpTable(Rule& rule);
+  void IpTablesWrapper::cleanupIpTablesModule() {}
 
   /**
-   * @brief Delete an Rule from the ip tables map
+   * @brief Append chain to iptables
    *
-   * @param rule    Rule object
+   * @param chainName
+   * @param ruleNum
    */
-  void deleteRuleFromIpTable(Rule& rule);
+  void appendRuleToChain(std::string& chainName, Rule& rule);
 
   /**
-   * @brief Modify an existing Rule from the ip tables map
+   * @brief Delete chain from iptables
    *
-   * @param rule    Rule object
+   * @param chainName
    */
-  void modifyRuleFromIpTable(Rule& rule);
+  void deleteRuleFromChain(std::string& chainName, Rule& rule) {}
+
+  /**
+   * @brief Insert chain in iptables
+   *
+   * @param chainName
+   */
+  void insertRuleIntoChain(std::string& chainName, Rule& rule) {}
+
+  /**
+   * @brief Replace chain in iptables
+   *
+   * @param chainName
+   */
+  void replaceRuleInChain(std::string& chainName, Rule& rule) {}
+
+  /**
+   * @brief List all chains in iptables
+   *
+   */
+  void listAllRulesInChain(std::string& chainName) {}
+
+  /**
+   * @brief List chain rule in iptables
+   *
+   */
+  void listRuleInChain(std::string& chainName, Rule& rule) {}
 
  private:
+  void generateIptEntry(Rule& rule, ipt_entry& iptEntry);
+  void generateOffset(ipt_entry& iptEntry);
+  void generateSource(Address& source, ipt_entry& iptEntry);
+  void generateDestination(Address& destionation, ipt_entry& iptEntry);
+  int commitChangesToIpTables();
+
+  std::string tableName;
+  xtc_handle* xtcHandle;
   RuleMap ruleMap;
 };
 }  // namespace iptables
--- src/IpTablesWrapper.cpp
+++ src/IpTablesWrapper.cpp
@@ -1,11 +1,93 @@
 #include "iptables/IpTablesWrapper.hpp"
 
+#include <string.h>
+
 using namespace iptables;
 
-void IpTablesWrapper::listRulesFromIpTable() {}
+void IpTablesWrapper::initializeIpTablesModule() {
+  this->xtcHandle = iptc_init(tableName.c_str());
+}
 
-void IpTablesWrapper::addRuleToIpTable(Rule& rule) {}
+void IpTablesWrapper::cleanupIpTablesModule() {
+  iptc_free(this->xtcHandle);
+}
 
-void IpTablesWrapper::deleteRuleFromIpTable(Rule& rule) {}
+void IpTablesWrapper::appendRuleToChain(std::string& chainName, Rule& rule) {
+  struct ipt_entry iptEntry;
+  generateIptEntry(rule, iptEntry);
 
-void IpTablesWrapper::modifyRuleFromIpTable(Rule& rule) {}
+  iptc_append_entry(chainName.c_str(), (struct ipt_entry*)&iptEntry, xtcHandle);
+  commitChangesToIpTables();
+}
+
+void IpTablesWrapper::deleteRuleFromChain(std::string& chainName, Rule& rule) {
+  ipt_chainlabel chainLabel;
+  unsigned char* matchMask;
+
+  struct ipt_entry iptEntry;
+  generateIptEntry(rule, iptEntry);
+
+  iptc_delete_entry(chainLabel, (struct ipt_entry*)&iptEntry, matchMask, xtcHandle);
+  commitChangesToIpTables();
+}
+
+void IpTablesWrapper::insertRuleIntoChain(std::string& chainName, Rule& rule) {
+  unsigned int ruleNum;
+  struct ipt_entry iptEntry;
+  generateIptEntry(rule, iptEntry);
+
+  iptc_insert_entry(chainName.c_str(), (struct ipt_entry*)&iptEntry, ruleNum, xtcHandle);
+  commitChangesToIpTables();
+}
+
+void IpTablesWrapper::replaceRuleInChain(std::string& chainName, Rule& rule) {
+  unsigned int ruleNum;
+  struct ipt_entry iptEntry;
+  generateIptEntry(rule, iptEntry);
+
+  iptc_replace_entry(chainName.c_str(), (struct ipt_entry*)&iptEntry, ruleNum, xtcHandle);
+  commitChangesToIpTables();
+}
+
+void listAllRulesInChain(std::string& chainName) {}
+
+void IpTablesWrapper::listRuleInChain(std::string& chainName, Rule& rule) {
+  unsigned char* matchMask;
+  struct ipt_entry iptEntry;
+  generateIptEntry(rule, iptEntry);
+
+  iptc_check_entry(chainName.c_str(), (struct ipt_entry*)&iptEntry, matchMask, xtcHandle);
+  commitChangesToIpTables();
+}
+
+void IpTablesWrapper::generateIptEntry(Rule& rule, ipt_entry& iptEntry) {
+  generateOffset(iptEntry);
+
+  Address source = rule.getSourceAddress();
+  generateSource(source, iptEntry);
+
+  Address destination = rule.getSourceAddress();
+  generateDestination(destination, iptEntry);
+}
+
+void IpTablesWrapper::generateOffset(ipt_entry& iptEntry) {
+  iptEntry.target_offset = sizeof(xt_entry_target);
+  iptEntry.next_offset = iptEntry.target_offset + sizeof(xt_entry_target);
+}
+
+void IpTablesWrapper::generateSource(Address& source, ipt_entry& iptEntry) {
+  iptEntry.ip.src.s_addr;
+  iptEntry.ip.smsk.s_addr;
+  iptEntry.ip.invflags;
+}
+
+void IpTablesWrapper::generateDestination(Address& destination, ipt_entry& iptEntry) {
+  iptEntry.ip.dst.s_addr;
+  iptEntry.ip.dmsk.s_addr;
+  iptEntry.ip.invflags;
+}
+
+int IpTablesWrapper::commitChangesToIpTables() {
+  int errorCode = iptc_commit(xtcHandle);
+  return errorCode;
+}
